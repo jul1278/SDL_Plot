e460d7e1fe2f4d778aa0659df1d6c3458f67ecab
Julian Pattie <jul1278@gmail.com>
2017-10-18 19:23:09 +1000

    drawing plot axis + refactoring

--- SDLPlot.h
+++ SDLPlot.h
@@ -1,40 +1,131 @@
 // ISDLPlot
+#include <memory>
+#include <vector>
 
 #include "SDL.h"
 
+//----------------------------------------------------------------------------------------------------------------------
+// Name: SDLPlotConfiguration
+// Desc:
+//----------------------------------------------------------------------------------------------------------------------
 struct SDLPlotConfiguration {
     bool grid; 
 
-    unsigned int gridSpacingX; 
-    unsigned int gridSpacingY; 
+    float gridSpacingX; 
+    float gridSpacingY; 
+
+    int xMinLimitLeft; 
+    int xMaxLimitLeft;
+    
+    int yMinLimitLeft;
+    int yMaxLimitLeft; 
+    
+    int xMinLimitRight; 
+    int xMaxLimitRight;
+    
+    int yMinLimitRight;
+    int yMaxLimitRight; 
+
+    int plotWidth;
+    int plotHeight; 
+
+    float topMargin;
+    float bottomMargin; 
+
+    float leftMargin;
+    float rightMargin; 
+    
+    std::string plotTitle; 
+
+    std::string xTitleLeft;
+    std::string xTitleRight;
+
+    std::string yTitle;
 
     std::shared_ptr<std::vector<int>> xData; 
     std::shared_ptr<std::vector<int>> yData;
 
-    int xMinLimit; 
-    int xMaxLimit;
-    
-    int yMinLimit;
-    int yMaxLimit; 
+    SDLPlotConfiguration() {
+        // TODO: set defaults here
+    }
 }; 
 
-// ISDLPlot
-class ISDLPlot {
+//----------------------------------------------------------------------------------------------------------------------
+// Name: SDLPlotConfiguration
+// Desc:
+//----------------------------------------------------------------------------------------------------------------------
+class SDLPlot {
 
     SDL_Renderer* renderer; 
+    SDL_Texture* texture;
+    
     SDLPlotConfiguration plotConfiguration; 
 
 public:
 
-    ISDLPlot(SDL_Renderer* renderer, const SDLPlotConfiguration& configuration) 
-        : renderer(renderer), plotConfiguration(configuration)
-    {
+    //------------------------------------------------------------------------------------------------------------------
+    // Name:
+    // Desc:
+    //------------------------------------------------------------------------------------------------------------------
+    SDLPlot(SDL_Renderer* renderer, SDL_Texture* texture, const SDLPlotConfiguration& configuration) 
+        : renderer(renderer), texture(texture), plotConfiguration(configuration) {}
 
+    ~SDLPlot() {}
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Name:
+    // Desc:
+    //------------------------------------------------------------------------------------------------------------------
+    void Draw() {
+
+        if (!this->ValidateConfig()) {
+            return;
+        }
+
+        SDL_SetRenderTarget(this->renderer, this->texture); 
+        SDL_RenderClear(this->renderer); 
+
+        SDL_SetRenderDrawColor(this->renderer, 0xFF, 0xFF, 0xFF, 0xFF); 
+
+        // draw y axis
+        auto y2 = this->plotConfiguration.plotHeight - this->plotConfiguration.topMargin; 
+        SDL_RenderDrawLine(this->renderer, this->plotConfiguration.leftMargin, this->plotConfiguration.topMargin, this->plotConfiguration.leftMargin, y2); 
+
+        // draw x axis 
+        auto x2 = this->plotConfiguration.plotWidth - this->plotConfiguration.rightMargin; 
+        auto y = this->plotConfiguration.plotHeight - this->plotConfiguration.bottomMargin; 
+        SDL_RenderDrawLine(this->renderer, this->plotConfiguration.leftMargin, y, x2, y); 
+
+        // draw grid
+
+        // draw titles 
+
+        // draw data onto plot
+
+        // TODO: draw annotation/captions etc
+
+        SDL_RenderCopy(this->renderer, this->texture, nullptr, nullptr); 
     }
 
-    ~ISDLPlot() {}
+private:
 
-    void Draw() {}
+    //------------------------------------------------------------------------------------------------------------------
+    // Name:
+    // Desc:
+    //------------------------------------------------------------------------------------------------------------------
+    bool ValidateConfig() {
+        
+        // TODO: validate config info
+        if (this->texture == nullptr) {
+            return false; 
+        }
+
+        if (this->renderer == nullptr) {
+            return false; 
+        } 
+
+        return true; 
+    }
 };
 
 
--- SDLPlotMain.cpp
+++ SDLPlotMain.cpp
@@ -3,277 +3,125 @@
 #include <exception>
 #include <cstring>
 
-#include "CsvImport.h"
+#include "PlotUtility.h"
+
+//#include "CsvImport.h"
+#include "SDLPlot.h"
 #include "SDL.h"
 
 #undef main
 
-/* SDL interprets each pixel as a 32-bit number, so our masks must depend
-    on the endianness (byte order) of the machine */
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    const uint32_t rmask = 0xff000000;
-    const uint32_t gmask = 0x00ff0000;
-    const uint32_t bmask = 0x0000ff00;
-    const uint32_t amask = 0x000000ff;
-#else
-    const uint32_t rmask = 0x000000ff;
-    const uint32_t gmask = 0x0000ff00;
-    const uint32_t bmask = 0x00ff0000;
-    const uint32_t amask = 0xff000000;
-#endif
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+//  SetupSDL
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+struct SDLInfo {
+    SDL_Renderer* renderer; 
+    SDL_Window* window; 
+    bool isError; 
 
+    void CleanUp() {
+        SDL_DestroyWindow(this->window);
+        SDL_DestroyRenderer(this->renderer);
+        SDL_Quit(); 
+    }
 
-// DrawGridInfo
-struct DrawGridInfo {
-    uint32_t color;
-    
-    unsigned int x; 
-    unsigned int y; 
-
-    unsigned int height; 
-    unsigned int width; 
-    
-    unsigned int xCount; 
-    unsigned int yCount;
-    
-    bool dotted;
+    ~SDLInfo() {
+        this->CleanUp(); 
+    }
 };
 
-// DrawCandleInfo
-struct DrawCandleInfo {
-
-    uint32_t color; 
-
-    unsigned int x; 
-    unsigned int y; 
-
-    unsigned int candleTop;
-    unsigned int candleBottom;
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+// Name: SetupSDL
+// Desc: 
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+SDLInfo SetupSDL(int windowWidth, int windowHeight) {
     
-    unsigned int width; 
-
-    unsigned int topWickHeight; 
-    unsigned int bottomWickHeight;   
-}; 
-
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-// Name: DrawCandle
-// Desc:
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-void DrawCandle(SDL_Renderer* renderer, const DrawCandleInfo& candleInfo) {
-
-}
-
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-// Name: DrawDottedLine
-// Desc:
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-void DrawDottedLine(
-    SDL_Renderer* renderer, 
-    const unsigned int color, 
-    int x1, 
-    int y1, 
-    int x2, 
-    int y2, 
-    const unsigned int period, 
-    const unsigned int cycle) {
-
-    // cant draw this
-    if (x1 == x2 && y1 == y2) {
-        return; 
-    }
-
-    // Always draw left to right/top to bottom so swap x's and y's if we need to
-    if (x1 > x2) {
-        std::swap(x1, x2); 
-    }
-
-    if (y1 > y2) {
-        std::swap(y1, y2); 
-    }
-
-    auto xDiff = (x2 - x1) > 0 ? (x2 - x1) : 1; 
-    auto yDiff = (y2 - y1) > 0 ? (y2 - y1) : 1; 
-
-    auto surface = SDL_CreateRGBSurface(0, xDiff, yDiff, 32, rmask, gmask, bmask, amask); 
-        
-    if (surface == nullptr) {
-        std::cout << "SDL_CreateRGBSurface failed\n"; 
-        return; 
-    }
-
-    SDL_LockSurface(surface);
-
-    auto arrSize = xDiff * yDiff;
-    auto pixelData = static_cast<uint32_t*>(surface->pixels); 
-
-    memset(pixelData, 0x00, arrSize * sizeof(uint32_t) ); 
-    unsigned int counter = 0; 
-
-    if (x1 == x2 || y1 == y2) {
-
-        // perfectly vertical line
-        auto indexTop = (y1 == y2) ? x2 - x1 : y2 - y1; 
-
-        for (auto index = 0; index < indexTop; index++) {
-            
-            if (index < arrSize && counter < cycle) {
-                pixelData[index] = color; 
-            }
-
-            counter++;
-            if (counter > period) {
-                counter = 0; 
-            }
-        }
-
-    } else {
-        
-        // sorta bresenhams line algorithm 
-        auto m = (float) yDiff / xDiff;
-        unsigned int yCurr = 0; 
-
-        for (auto x = 0; x < xDiff; x++) {
-
-            // calculate predicted value - 
-            auto error = m * x - (float) yCurr;
-
-            if (m == 0) {
-                error = y2 - yCurr; // ????? 
-            }
-
-            if (error >= 1.0f) {
-                while (error >= 1.0f) {
-                    
-                    auto index = (yCurr * xDiff) + x;
-                
-                    // draw pixel
-                    if (index < arrSize && counter < cycle) {
-                        pixelData[index] = color; 
-                    }
-        
-                    yCurr++;
-                    counter++;
-                    
-                    if (counter > period) {
-                        counter = 0;
-                    }
-
-                    error -= 1.0f; 
-                }
-            } else {
-
-                auto index = (yCurr * xDiff) + x;
-                
-                // draw pixel
-                if (index < arrSize && counter < cycle) {
-                    pixelData[index] = color; 
-                }
-
-                counter++; 
-
-                if (counter > period) {
-                    counter = 0;
-                }
-            }
-        }
-    }
-
-    SDL_UnlockSurface(surface);
-
-    auto texture = SDL_CreateTextureFromSurface(renderer, surface); 
-
-    SDL_Rect rect = {x1, y1, xDiff, yDiff}; 
-    SDL_RenderCopy(renderer, texture, NULL, &rect);
-
-    SDL_FreeSurface(surface);
-    SDL_DestroyTexture(texture); 
-
-    return;
-}
-
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-// Name: DrawGrid
-//
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-void DrawGrid(SDL_Renderer* renderer, const DrawGridInfo& drawGridInfo) {
-
-    SDL_SetRenderDrawColor(renderer, drawGridInfo.color >> 24, (drawGridInfo.color & 0x00ff0000) >> 16, (drawGridInfo.color & 0x0000ff00) >> 8, (drawGridInfo.color & 0x000000ff)); 
-
-    unsigned int xSpacing = drawGridInfo.width / drawGridInfo.xCount; 
-    unsigned int ySpacing = drawGridInfo.height / drawGridInfo.yCount;
-
-    for (auto x = xSpacing; x < drawGridInfo.width; x += xSpacing) {
-        if (drawGridInfo.dotted) {
-            DrawDottedLine(renderer, drawGridInfo.color, x, drawGridInfo.y, x, drawGridInfo.height, 10, 5);
-        } else {
-            SDL_RenderDrawLine(renderer, x, drawGridInfo.y, x, drawGridInfo.height); 
-        }
-    }
-
-    for (auto y = ySpacing; y < drawGridInfo.height; y += ySpacing) {
-        
-        if (drawGridInfo.dotted) {
-            DrawDottedLine(renderer, drawGridInfo.color, drawGridInfo.x, y, drawGridInfo.width, y, 10, 5);
-        } else {
-            SDL_RenderDrawLine(renderer, drawGridInfo.x, y, drawGridInfo.width, y);
-        }
-    }
-}
-
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-// Name: DrawGrid
-//
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-int main(int argc, char* argv[]) {
-
+    SDLInfo info; 
+    
     // fix for when debugging in gdb causes app to crash
     SDL_SetHint(SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        return 0;
+        
+        info.isError = true; 
+        return info;
     }
+
+    info.window = SDL_CreateWindow("SDL Plot", 100, 100, windowWidth, windowHeight, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+
+    if (info.window == nullptr) {
+        SDL_Quit();
+        
+        info.isError = true; 
+        return info;
+    }
+
+    info.renderer = SDL_CreateRenderer(info.window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
+
+    if (info.renderer == nullptr) {
+        SDL_DestroyWindow(info.window);
+        SDL_Quit();
+        
+        info.isError = true; 
+        return info;
+    } 
+
+    info.isError = false; 
+    return info; 
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+// Name: main
+// Desc:
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+int main(int argc, char* argv[]) {
 
     int windowWidth = 640;
     int windowHeight = 480; 
 
-    auto window = SDL_CreateWindow("SDL Plot", 100, 100, windowWidth, windowHeight, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    auto sdlInfo = SetupSDL(windowWidth, windowHeight);
 
-    if (window == nullptr) {
-        SDL_Quit();
-        return 0;
+    if (sdlInfo.isError) {
+        return 0; 
     }
 
-    auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
 
-    if (renderer == nullptr) {
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 0;
-    }
 
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
-    SDL_RenderClear(renderer);
-
-    DrawGridInfo drawGridInfo {0xffffffff, 10, 10, windowHeight - 10, windowWidth - 10, 5, 5, true}; 
-    DrawGrid(renderer, drawGridInfo); 
-    
-    SDL_RenderPresent(renderer);
-    
     while(1) {
+
         SDL_Event event;
         
         if(SDL_PollEvent(&event)) {
 
             if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
-                SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
-                SDL_RenderClear(renderer);
+                SDL_SetRenderDrawColor(sdlInfo.renderer, 0x00, 0x00, 0x00, 0xff);
+                SDL_RenderClear(sdlInfo.renderer);
 
-                SDL_GetWindowSize(window, &windowWidth, &windowHeight); 
-                drawGridInfo = DrawGridInfo{0xffffffff, 10, 10, windowHeight - 10, windowWidth - 10, 5, 5, true}; 
-  
-                DrawGrid(renderer, drawGridInfo);
+                SDL_GetWindowSize(sdlInfo.window, &windowWidth, &windowHeight); 
 
-                SDL_RenderPresent(renderer);
+                // SDL_RenderPresent(sdlInfo.renderer);
+
+                auto texture = SDL_CreateTexture(sdlInfo.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, windowWidth, windowHeight);
+                
+                // TODO: 
+                SDLPlotConfiguration config; 
+                config.leftMargin = 50; 
+                config.rightMargin = 50;
+                config.topMargin = 50;
+                config.bottomMargin = 50; 
+                config.plotWidth = windowWidth;
+                config.plotHeight = windowHeight; 
+            
+                SDLPlot plot(sdlInfo.renderer, texture, config); 
+                plot.Draw(); 
+            
+                // detach render target
+                SDL_SetRenderTarget(sdlInfo.renderer, nullptr); 
+            
+                // now render texture to the screen
+                SDL_RenderCopyEx(sdlInfo.renderer, texture, nullptr, nullptr, 0, nullptr, SDL_FLIP_NONE); 
+                SDL_RenderPresent(sdlInfo.renderer);
+
+                SDL_DestroyTexture(texture);                 
             }
 
             if (event.type == SDL_QUIT) {
@@ -281,11 +129,8 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-    
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
-    SDL_Quit(); 
 
+    
     return 0; 
 }
 
